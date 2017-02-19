93942f7887494d090d85a0c7b210348a7004bcc7
Jianyu Zhou <Jianyu Zhou>
2017-02-19 14:09:53 +0800

    add scripts

--- tmp.py
+++ tmp.py
@@ -1,9 +1,6 @@
-# -*-coding:utf-8-*-
-lines = open('shiji_trans_split.txt','r').readlines()
-new_lines = []
-for line in lines:
-	small_lines = line.split(',')
-	for i in range(len(small_lines)-1):
-		small_lines[i] = small_lines[i]+'，\n'
-	new_lines+=small_lines
-open('shiji_trans_split2.txt','w').writelines(new_lines)
+#-*-coding:utf-8 -*-
+lines = open(u'白话二十四史之18【宋史（下）、辽史.txt','r').readlines()
+liao = lines[10014:len(lines)]
+songxia = lines[0:10014]
+open('songshixia.txt','w').writelines(songxia)
+open('liaoshi.txt','w').writelines(liao)
